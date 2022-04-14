669241bb958eb8f8657181e30bc7edde40fcbd1a
KeshavAbhishek <crystaled2003@gmail.com>
2022-04-14 18:46:50 +0530

    UI RE-DESIGNED

--- node.js
+++ node.js
@@ -3,8 +3,8 @@ var expand = 0;
 function menuClicked(){
     if(expand==0){
 
-        document.getElementById('navbar').classList.remove('navcssRemove');
-        document.getElementById('navbar').classList.add('navcss');
+        document.getElementById('unorderedList').classList.remove('navcssRemove');
+        document.getElementById('unorderedList').classList.add('navcss');
         expand+=1;
 
         document.getElementById('bar-1').style.transform='rotate(45deg)';document.getElementById('bar-1').style.left='7%';document.getElementById('bar-4').style.transform='rotate(-45deg)';document.getElementById('bar-4').style.left='40%';document.getElementById('bar-2').style.width='0%';document.getElementById('bar-5').style.width='0%';document.getElementById('bar-3').style.transform='rotate(-45deg)';document.getElementById('bar-3').style.left='7%';document.getElementById('bar-6').style.transform='rotate(45deg)';document.getElementById('bar-6').style.left='40%';
@@ -12,8 +12,8 @@ function menuClicked(){
         document.getElementById('bar-1').style.transition='all 0.5s ease';document.getElementById('bar-2').style.transition='all 0.5s ease';document.getElementById('bar-3').style.transition='all 0.5s ease';document.getElementById('bar-4').style.transition='all 0.5s ease';document.getElementById('bar-5').style.transition='all 0.5s ease';document.getElementById('bar-6').style.transition='all 0.5s ease';
     }
     else{
-        document.getElementById('navbar').classList.add('navcssRemove');
-        document.getElementById('navbar').classList.remove('navcss');
+        document.getElementById('unorderedList').classList.add('navcssRemove');
+        document.getElementById('unorderedList').classList.remove('navcss');
         expand-=1;
 
         document.getElementById('bar-1').style.transform='rotate(0deg)';document.getElementById('bar-1').style.left='25%';document.getElementById('bar-4').style.transform='rotate(0deg)';document.getElementById('bar-4').style.left='25%';document.getElementById('bar-2').style.width='50%';document.getElementById('bar-5').style.width='50%';document.getElementById('bar-3').style.transform='rotate(0deg)';document.getElementById('bar-3').style.left='25%';document.getElementById('bar-6').style.transform='rotate(0deg)';document.getElementById('bar-6').style.left='25%';
@@ -24,8 +24,8 @@ function menuClicked(){
 
 window.addEventListener('resize',()=>{
     if(window.innerWidth>700){
-        document.getElementById('navbar').classList.remove('navcssRemove');
-        document.getElementById('navbar').classList.remove('navcss');
+        document.getElementById('unorderedList').classList.remove('navcssRemove');
+        document.getElementById('unorderedList').classList.remove('navcss');
     }
 });
 
